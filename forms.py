f1701031defc63ffa45aa9b57687fc94173edd0f
AJL <ajlabozz@yahoo.com>
2018-11-26 20:41:16 -0500

    Updating Prod to use as new repository

--- forms.py
+++ forms.py
@@ -1,59 +1,113 @@
+#The Connect
+from flask import Flask, render_template, redirect, url_for, flash, request
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, SelectField, DecimalField
-from wtforms.validators import DataRequired
+from flask_login import LoginManager, UserMixin, login_user, logout_user, \
+    current_user, login_required
+from werkzeug.urls import url_parse
+from werkzeug.security import check_password_hash, generate_password_hash
+from wtforms import Form, StringField, SubmitField, IntegerField, PasswordField, SelectField, DecimalField, TextAreaField, DateField, validators
+import pymysql
+from flask_user import roles_required   # we will have three roles; admin, intern, sponsor
+import sys
+from wtforms.validators import DataRequired, NumberRange, EqualTo, Email
 
+#login
 class loginForm(FlaskForm):
-        username = StringField('Username:',validators=[DataRequired()])
-        password = StringField('Password:', validators=[DataRequired()])
-        submit = SubmitField('Submit')
-'''       
+    email = StringField('Email',validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')	
+	
+#new internship
 class createInternship(FlaskForm):
-	email = StringField('Email address', validators=[DataRequired()])
-	address = StringField('Address: ',validators=[DataRequired()])
-	address2 = StringField('Address 2: ',validators=[DataRequired()])
-	city = StringField('City: ',validators=[DataRequired()])
-	state = SelectField('State',choices=[('ct', 'Connecticut'), ('ma', 'Massachussets'), ('ny', 'New York')])
-	zipcode = StringField('Zip' ,validators=[DataRequired()]) 
-	major = StringField('Desired Major: ',validators=[DataRequired()])
-	gpa = DecimalField('Minimum GPA: ',places =1,validators=[DataRequired()])
-	pay = DecimalField('Pay Rate : $',places=2)
-	description = TextAreaField('Internship description', validators=[DataRequired()])
-	submit = SubmitField('Submit')'''
 
-'''	
+	heading = StringField('Internship Title')
+	description = TextAreaField('Internship description', validators=[DataRequired()])
+	startDate = DateField('Start Date', format='%m-%d-%Y', validators=[DataRequired()])
+	endDate = DateField('End Date', format='%m-%d-%Y')
+	major = StringField('Desired Major',validators=[DataRequired()])
+	gpa = DecimalField('Minimum GPA',places =1,validators=[DataRequired()])
+	pay = DecimalField('Pay Rate $',places=2)
+	public = SelectField('Make internship publicly viewable or by referral only',choices=[(1, 'Public'),(0, 'Referral-only')])
+	submit = SubmitField('Submit')
+
+#new sponsor
 class createSponsor(FlaskForm):
-	email = StringField('Email address ', validators=[DataRequired()])
-	#emailConfirm = EmailField('Re-enter Email address ', [validators.DataRequired(),validators.Equalto('confirm ') ,validators.Email()])
-	password = PasswordField('Password ', [validators.DataRequired(),validators.EqualTo('confirm ', message='Passwords must match')])
+    sponsorID = StringField('User ID', validators=[DataRequired()])
+    email = StringField('Email Address', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+    company = StringField('Organization Name', validators=[DataRequired()])
+    website = StringField('Organization website', validators=[DataRequired()])
+    phone = StringField('Organization Contact Phone', validators=[DataRequired()])
+    address = StringField('Organization Address',validators=[DataRequired()])
+    city = StringField('Organization City',validators=[DataRequired()])
+    state = SelectField('Organization State',choices=[('ct', 'Connecticut'), ('ma', 'Massachussets'), ('ny', 'New York')])
+    zipcode = StringField('Organization Zip' ,validators=[DataRequired()])
+    description = TextAreaField('Organization description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+#new student
+class createStudent(FlaskForm):
+    studentID = StringField('Student ID', validators=[DataRequired()])
+    email = StringField('Email address', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
+    confirm = PasswordField('Repeat Password')
+    fname = StringField('First Name', validators=[DataRequired()])
+    lname = StringField('Last Name', validators=[DataRequired()])
+    phone = StringField('Phone', validators=[DataRequired()])
+    address = StringField('Address',validators=[DataRequired()])
+    address2 = StringField('Address 2')
+    city = StringField('City',validators=[DataRequired()])
+    state = SelectField('State',choices=[('ct', 'Connecticut'), ('ma', 'Massachussets'), ('ny', 'New York')])
+    zipcode = StringField('Zip' ,validators=[DataRequired()])
+    major = StringField('Major',validators=[DataRequired()])
+    gpa = DecimalField('GPA',places=1,validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+#new admin
+class createAdmin(FlaskForm):
+	studentID = StringField('Student ID', validators=[DataRequired()])
+	email = StringField('Email address', validators=[DataRequired()])
+	password = PasswordField('Password', validators=[DataRequired(), EqualTo('confirm', message='Passwords must match')])
 	confirm = PasswordField('Repeat Password')
-	address = Stringfield('Address: ',validators=[DataRequired()])
-	address2 = StringField('Address 2: ',validators=[DataRequired()])
-	city = StringField('City: ',validators=[DataRequired()])
+	fname = StringField('First Name', validators=[DataRequired()])
+	lname = StringField('Last Name', validators=[DataRequired()])
+	phone = StringField('Phone', validators=[DataRequired()])
+	address = StringField('Address',validators=[DataRequired()])
+	address2 = StringField('Address 2')
+	city = StringField('City',validators=[DataRequired()])
 	state = SelectField('State',choices=[('ct', 'Connecticut'), ('ma', 'Massachussets'), ('ny', 'New York')])
 	zipcode = StringField('Zip' ,validators=[DataRequired()])
-	description = TextAreaField('Organization description', validators=[DataRequired()])
-	submit = SubmitField('Submit')'''
-
-'''
-class createStudent(FlaskForm):
-	studentID = StringField('Student ID', validators=[DataRequired()])
-	email = StringField('Email address ', validators=[DataRequired()])
-	#emailConfirm = EmailField('Re-enter Email address ', [validators.DataRequired(),validators.Equalto('confirm ') ,validators.Email()])
-	password = PasswordField('Password ', [validators.DataRequired(),validators.EqualTo('confirm ', message='Passwords must match')])
-	confirm = PasswordField('Repeat Password')
-	address = Stringfield('Address: ',validators=[DataRequired()])
-	address2 = StringField('Address 2: ',validators=[DataRequired()])
-	city = StringField('City: ',validators=[DataRequired()])
-	state = SelectField('State ',choices=[('ct', 'Connecticut'), ('ma', 'Massachussets'), ('ny', 'New York')])
-	zipcode = StringField('Zip ' ,validators=[DataRequired()])
-	major = StringField('Major ',validators=[DataRequired()])
-	gpa = DecimalField('GPA ',places=1,validators=[DataRequired()])
-	submit = SubmitField('Submit')'''
-
-'''	
+	submit = SubmitField('Submit')	
+ 	
+#error report
 class createTicket(FlaskForm):
 	errType = SelectField(u'Error Type', choices=[('', ''), ('', ''), ('', '')])
-	email = EmailField('Email address ', [validators.DataRequired(), validators.Email()])
+	email = StringField('Email address', validators=[DataRequired(), Email()])
 	errDescription = TextAreaField('Error Description ' , validators=[DataRequired()])
+	submit = SubmitField("Submit")
 	
-'''
+#Contact	
+class contactForm(FlaskForm):
+	name = StringField("Name")
+	email = StringField("Email")
+	subject = StringField("Subject")
+	message = TextAreaField("Message")
+	submit = SubmitField("Send")
+
+	
+#Internship Search Form
+
+
+#Profile Edit
+
+
+#Internship Edit
+
+
+#Internship Application
+
+	
+#
--- main.py
+++ main.py
@@ -1,123 +1,432 @@
-#The Connect
-from flask import Flask, render_template, redirect, url_for
+# The Connect
+from flask import Flask, render_template, redirect, url_for, flash, request
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, IntegerField, PasswordField, SelectField
-from wtforms.validators import DataRequired, NumberRange
+from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required, current_user, \
+    login_required
+from werkzeug.urls import url_parse
+from werkzeug.security import check_password_hash, generate_password_hash
+from wtforms import Form, StringField, SubmitField, IntegerField, PasswordField, SelectField, DecimalField, \
+    TextAreaField, validators
+from wtforms.validators import DataRequired, NumberRange, EqualTo, Email
 import pymysql
-from flask_user import roles_required   # we will have three roles; admin, intern, sponsor
+from flask_user import roles_required  # we will have three roles; admin, intern, sponsor
+from forms import *
+import sys
+
+
+class User(UserMixin):
+    def __init__(self, userID, email, password, role):
+        self.id = userID
+        self.email = email
+        self.pass_hash = generate_password_hash(password)
+        print(self.pass_hash, file=sys.stderr)
+        self.role = role
+
+    def getID(self):
+        return self.id
+
+    def getEmail(self):
+        return self.email
+
+    def getRole(self):
+        return self.role
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'TheConnect is the best string'    #various flask extensions need a "secret key"
-
-bootstrap = Bootstrap(app)                                    #invokes bootstrap
-moment = Moment(app)#invokes bootstrap
-
-db = pymysql.connect(host='35.231.51.121',user='root',password='connect1234', db ='theConnect')
+app.config['SECRET_KEY'] = 'TheConnect is the best string'  # various flask extensions need a "secret key"
+bootstrap = Bootstrap(app)  # invokes bootstrap
+moment = Moment(app)  # invokes bootstrap
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+db = pymysql.connect(host='35.231.51.121', user='root', password='connect1234', db='theConnect')
 c = db.cursor()
 
 
+# user roles
+def is_admin():
+    if current_user:
+        if current_user.role == 'admin':
+            return True
+        else:
+            return False
+    else:
+        print('User not authenticated.', file=sys.stderr)
 
-class loginForm(FlaskForm):
-        username = StringField('Username:',validators=[DataRequired()])
-        password = StringField('Password:', validators=[DataRequired()])
-        submit = SubmitField('Log in')
-        
-@app.route('/base')                                           #This is the base.html that every webpages uses.
+
+def is_sponsor():
+    if current_user:
+        if current_user.role == 'sponsor':
+            return True
+        else:
+            return False
+    else:
+        print('User not authenticated.', file=sys.stderr)
+
+
+def is_student():
+    if current_user:
+        if current_user.role == 'student':
+            return True
+        else:
+            return False
+    else:
+        print('User not authenticated.', file=sys.stderr)
+
+
+# Login manager uses this function to manage user sessions.
+# Function does a lookup by id and returns the User object if
+# it exists, None otherwise.		
+@login_manager.user_loader
+def load_user(user):
+    return user
+
+
+@app.route('/base')  # This is the base.html that every webpages uses.
 def base():
     return render_template('base.html')
 
 
 @app.route('/test')
 def test():
-    return render_template('test.html')
+    form = loginForm()
+    return render_template('test.html', form=form)
+
 
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    title = "404 - page not found"
+    nav1 = "Home"
+    logo_link = "/"
+    return render_template('404.html', title=title, nav1=nav1, logo_link=logo_link), 404
 
 
 @app.errorhandler(500)
 def internal_server_error(e):
+    title = "505 - internal server error"
+    nav1 = "Home"
+    logo_link = "/"
     return render_template('500.html'), 500
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     title = "TheConnect"
-    nav1= "Login"
-    nav1_link = '/login'
-    return render_template('landing.html', title=title, nav1=nav1, nav1_link=nav1_link,)
+    logo_link = "/"
 
+    if current_user.is_authenticated:
+        return redirect(url_for('profile'))
+	
+    form = loginForm()
+    if form.validate_on_submit():
+        email = form.email.data
+        c.execute('SELECT * FROM User WHERE UserID = %s;' %(email))
+        data = c.fetchall()
 
+        for row in data:
+            userID,email,password,role = row[0],row[1],row[2],row[3]
+		
+            user = User(userID,email,password,role)
+            valid_password = check_password_hash(user.pass_hash, form.password.data)
+            if user is None or not valid_password:
+                print('Invalid username or password', file=sys.stderr)
+                redirect(url_for('home'))
+            else:
+                login_user(user)
+                if user.getRole == 'Sponsor':
+                    return redirect(url_for('sponsor_profile'))
+                elif user.getRole == 'Faculty':
+                    return redirect(url_for('admin_home'))
+                else:
+                    return redirect(url_for('intern_profile'))
+    return render_template('landing.html', form=form, title=title, logo_link=logo_link)
+	
+'''
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    form = loginForm()
-    title = "Login"
-    nav1 = "Home"
-    nav2 = 'Register'
-    nav1_link = "/"
-    return render_template('login.html', form=form, title=title, nav1=nav1, nav1_link=nav1_link)
+	title = "Sign In"
+	logo_link = '/'
+
+	if current_user.is_authenticated:
+		return redirect(url_for('profile'))
+    
+	form = loginForm()
+	if form.validate_on_submit():
+		user = db[form.email.data]
+		
+		valid_password = check_password_hash(user.pass_hash, form.password.data)
+		if email is None or not valid_password:
+			print('Invalid username or password', file=sys.stderr)
+			redirect(url_for('home'))
+		else:
+			login_user(email)
+			return redirect(url_for('profile'))
+
+	return render_template('login.html', title=title, form=form, logo_link=logo_link)
+'''
 
 
-@app.route('/profile')
-def profile():
-    return render_template('profile.html')
+@app.route('/intern')
+def intern_profile():
+    title = "Profile"
+
+    # profile_pic = "..\static\img\s_profile.png"  testing out profile pic
+    c.execute('Select * from Student where UserID ')
+    data = c.fetchall()
+
+    for row in data:
+        f_name = row[1]
+        l_name = row[2]
+        degree = row[6]
+        gpa = row[7]
+        email = row[4]
+        phone = row[5]
+        
+    school = "Southern"
+    profile_pic = "https://raw.githubusercontent.com/scsu-csc330-400/blu-test/help_jason/Static/img/b.jpg?token=AoQ7TSJDqVpIdxBM_4hwk9J2QSluOd47ks5b7GhvwA%3D%3D"
+
+    return render_template('intern_profile.html', profile_pic=profile_pic, first_name=f_name, last_name=l_name, \
+                           degree=degree, school=school, gpa=gpa, email=email, phone=phone)
 
 
-@app.route('/admin/home')   #doesnt work yet, needs to define the class.
-@roles_required('admin')
+@app.route('/sponsor')
+def sponsor_profile():
+    title = "Profile"
+    # profile_pic = "..\static\img\s_profile.png"  testing out profile pic
+    profile_pic = None
+    f_name = "First Name"
+    l_name = "Last Name"
+    degree = "Business"
+    school = "Southern"
+    gpa = "4.2"
+    email = "boyv@southernct.edu"
+    phone = "203-911-9111"
+    profile_pic = "https://raw.githubusercontent.com/scsu-csc330-400/blu-test/help_jason/Static/img/b.jpg?token=AoQ7TSJDqVpIdxBM_4hwk9J2QSluOd47ks5b7GhvwA%3D%3D"
+
+    return render_template('sponsor_profile.html', profile_pic=profile_pic, first_name=f_name, last_name=l_name, \
+                           degree=degree, school=school, gpa=gpa, email=email, phone=phone)
+
+
+@app.route('/admin_home')  # doesnt work yet, needs to define the class.
+# @login_required
+# @roles_required('admin')
 def admin_home():
-    title = "admin home"
+    title = "Admin Home"
     return render_template('admin_home.html', title=title)
 
 
-@app.route('/admin/login')
+'''
+@app.route('/admin_login')
 def admin_login():
-    return render_template('admin_login.html')
+	title = "Admin Login"
+	logo_link = '/'
+	if current_user.is_authenticated:
+		return redirect(url_for('profile'))
+    
+	form = loginForm()
+	if form.validate_on_submit():
+		user = db[form.username.data]
+		valid_password = check_password_hash(user.pass_hash, form.password.data)
+		if user is None or not valid_password:
+			print('Invalid username or password', file=sys.stderr)
+			redirect(url_for('home'))
+		else:
+			login_user(user)
+			return redirect(url_for('profile'))
+
+	return render_template('admin_login.html', title=title, form=form, logo_link=logo_link)
+'''
 
 
-@app.route('/create/internship')
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route('/create_internship', methods=['GET', 'POST'])
+# @login_required
+# roles_required('admin','sponsor')
 def create_internship():
-    return render_template('create_internship.html')
+    form = createInternship()
+    title = "Internship"
+    logo_link = "/"
+
+    if form.validate_on_submit():
+        email = form.email.data
+        address = form.address.data
+        address2 = form.address2.data
+        city = form.city.data
+        state = form.state.data
+        zipcode = form.zipcode.data
+        startDate = form.startDate.data
+        endDate = form.endDate.data
+        major = form.major.data
+        gpa = form.gpa.data
+        pay = form.pay.data
+        description = form.description.data
+        approved = 0
+
+        c.execute('INSERT INTO Internships values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' % (
+        email, address, address2, city, state, zipcode, startDate, endDate, major, gpa, pay, description))
+
+        db.commit()
+        return render_template('successful_internship.html', title=title, nav1=nav1, logo_link=logo_link)
+
+    return render_template('create_internship.html', form=form, title=title, logo_link=logo_link)
 
 
-@app.route('/create/sponsor')
+@app.route('/create_sponsor', methods=['GET', 'POST'])
 def create_sponsor():
-    return render_template('create_sponsor.html')
+    form = createSponsor()
+    title = "Sponsor"
+    logo_link = "/"
+
+    if form.validate_on_submit():
+        role = "Sponsor"
+        sponsorID = form.sponsorID.data
+        email = form.email.data
+        password = form.password.data
+        company = form.company.data
+        website = form.website.data
+        phone = form.phone.data
+        address = form.address.data
+        city = form.city.data
+        state = form.state.data
+        zipcode = form.zipcode.data
+        description = form.description.data
+
+        c.execute('INSERT INTO User values("%s","%s","%s","%s")' % (sponsorID, email, password, role))
+        c.execute('INSERT INTO Sponsor values("%s","%s","%s","%s","%s","%s","%s","%s","%s")' % (
+        sponsorID, company, address, website, phone, zipcode, city, description, state))
+
+        db.commit()
+        return redirect(url_for('home'))
+    return render_template('create_sponsor.html', form=form, title=title, logo_link=logo_link)
 
 
-@app.route('/create/student')
+@app.route('/create_student', methods=['GET', 'POST'])
 def create_student():
-    return render_template('create_student.html')
+    form = createStudent()
+    title = "Student"
+    logo_link = "/"
+
+    if form.validate_on_submit():
+        role = "Student"
+        studentID = form.studentID.data
+        email = form.email.data
+        password = form.password.data
+        fname = form.fname.data
+        lname = form.lname.data
+        phone = form.phone.data
+        address = form.address.data
+        address2 = form.address2.data
+        city = form.city.data
+        state = form.state.data
+        zipcode = form.zipcode.data
+        major = form.major.data
+        gpa = form.gpa.data
+
+        c.execute('INSERT INTO User values("%s","%s","%s","%s")' % (studentID, email, password, role))
+        c.execute('INSERT INTO Student values("%s","%s","%s","%s","%s","%s","%s",%s,"%s","%s","%s","%s")' % (
+        studentID, fname, lname, address, email, phone, major, gpa, state, address2, city, zipcode))
+
+        db.commit()
+        return redirect(url_for('home'))
+
+    return render_template('create_student.html', form=form, title=title, logo_link=logo_link)
 
 
-@app.route('/create/ticket')
+@app.route('/create_ticket', methods=['GET', 'POST'])
 def create_ticket():
-    return render_template('create_ticket.html')
+    form = createTicket()
+    title = "Report Error"
+    logo_link = "/"
+    if form.validate_on_submit():
+        errType = form.errType.data
+        email = form.email.data
+        errDescription = form.errDescription.data
+        c.execute('INSERT INTO Error values("%s","%s","%s","%s",)' % (errType, email, errDescription))
+        db.commit()
+        return render_template('landing.html', form=form, title=title, nav1=nav1, logo_link=logo_link)
+    return render_template('create_ticket.html', form=form, title=title, logo_link=logo_link)
+
+
+# resume
+# resume_location = "..\static\img\s_resume.jpg"
+# @app.route('/user_resume')
+# def user_resume():
+#    title = "Resume"
+#    logo_link = "/"
+#    resume_location = "..\static\img\s_resume.jpg"
+#    return render_template('user_resume.html', title=title, resume=resume_location, logo_link=logo_link)
+
+# @app.route('/user/<user>')
+# def user_profile():
+#     title = "Profile"
+#     return render_template('sponsor_profile.html', title=title)
+
+@app.route('/success')
+def successful_internship():
+    title = "Success"
+    logo_link = "/"
+    return render_template('successful_internship.html', title=title, logo_link=logo_link)
+
+
+@app.route('/faq')
+def faq():
+    title = "FAQ"
+    logo_link = "/"
+    return render_template('faq.html', title=title, logo_link=logo_link)
+
+
+@app.route('/about')
+def about():
+    title = "About"
+    logo_link = "/"
+    return render_template('about.html', title=title, logo_link=logo_link)
+
+
+@app.route('/contact')
+def contact():
+    form = contactForm()
+    title = "Contact"
+    logo_link = "/"
+    return render_template('contact.html', form=form, title=title, logo_link=logo_link)
 
 
 @app.route('/help')
 def help():
-    return render_template('help.html')
+    title = "Help"
+    logo_link = "/"
+    return render_template('help.html', title=title, logo_link=logo_link)
 
 
 @app.route('/notifications')
 def notifications():
-    return render_template('notifications.html')
+    title = "Notifications"
+    return render_template('notifications.html', title=title)
 
 
-@app.route('/sponsor/profile')
-def sponsor_profile():
-    return render_template('sponsor_profile.html')
-
-@app.route('/about')
-def about():
-    title="about"
-    return render_template('about.html', title=title)
+@app.route('/user_resume')
+def user_resume():
+    title = "Resume"
+    logo_link = "/"
+    resume_location = "..\static\img\s_resume.jpg"
+    return render_template('user_resume.html', title=title, resume=resume_location, logo_link=logo_link)
 
 
-if __name__ == '__main__':                                   #You can run the main.py and type "localhost:8080" in your
-    app.run(host='0.0.0.0', port=8080, debug=True)           #broswer to test the main.py in your computer.
+@app.route('/register')
+def register():
+    title = "Register"
+    return render_template('register.html', title=title)
+
+
+@app.route('/search')
+def search():
+    return render_template('search.html')
+
+
+if __name__ == '__main__':  # You can run the main.py and type "localhost:8080" in your
+    app.run(host='0.0.0.0', port=8080, debug=True)  # broswer to test the main.py in your computer.
