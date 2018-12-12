46393356ed1a7d5bf47c3fee56c09c330f224778
Unknown <ajlabozz@yahoo.com>
2018-12-12 05:24:05 -0500

    Revert "kinda working"
    This reverts commit 2bf0dd7c4d01546fe99a5aae48f30108b979f4e7.

--- main.py
+++ main.py
@@ -581,5 +581,54 @@ def search_results(search):
 		return redirect(url_for('internships'))
 	return render_template('internships.html', data=data, form=form, logo_link=logo_link)
 
+@app.route('/view_internship/<postID>', methods=["GET","POST"])
+#login.required
+def viewInternship(postID):
+	logo_link = "/"
+	sql = ('SELECT * FROM Internship WHERE postID = %s' %(postID))
+	c.execute(sql)
+	data = c.fetchall()
+
+	for row in data:
+		company = row[0]
+		title = row[1]
+		body= row[2]
+		start = row[3]
+		end = row[4]
+		gpa = row[5]
+		pay = row[6]
+		post = postID
+	if request.method == "POST":
+		submitApplication()
+		
+	return render_template('view_internship.html', data=data, logo_link=logo_link, company=company, title=title, body=body, start=start, end=end, gpa=gpa, pay=pay, post=post)
+
+@app.route('/submit_application', methods=["GET","POST"])
+#login.required
+def submitApplication():
+	user = current_user.getID()
+	sql = ('SELECT * FROM Student WHERE UserID=%s' %(user))
+	c.execute(sql)
+	student_data = c.fetchall()
+	
+	applicationID = str(random.randrange(100000,1000000))
+	for row in student_data:
+		f_name = row[1]
+		l_name = row[2]
+		degree = row[6]
+		gpa = row[7]
+		phone = row[5]
+		interest = row[12]
+		availability = row[14]
+		bio = row[13]
+		postID = "2509"
+
+
+	sql = ('INSERT INTO Applicants values("%s","%s","%s","%s","%s","%s","%s","%s","%s","%s")' %(applicationID, f_name, l_name, degree, gpa, phone, interest, availability, bio, postID))
+	c.execute(sql)
+	db.commit()
+
+	return render_template('internships.html', user=user, id=id, data=data, form=form, logo_link=logo_link)
+
 if __name__ == '__main__':  # You can run the main.py and type "localhost:8080" in your
     app.run(host='0.0.0.0', port=8080, debug=True)  # broswer to test the main.py in your computer.
